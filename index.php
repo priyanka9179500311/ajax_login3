a50c1bf492a67aad1c84e70d1783eb71a5601473
priyanka9179500311 <py63158@gmail.com>
2020-11-01 16:55:16 +0530

    ajaxlogin

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Bootstrap 4 Example</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+		<style>
+			.a_cont{
+				height:500px;
+				border:1px solid red;
+			}
+			.blue{
+				background-color:blue;
+			}
+			.green{
+				background-color:green;
+			}
+		</style>
+	</head>
+	<body>
+
+		<div class="container-fluid a_cont">
+			<div class="col-sm-4 col-mg-6 col-lg-4 col-xl-6 blue">A</div>
+			<div class="clo-sm-8 col-mg-6 col-lg-8 col-xl-6 green">B</div>
+			
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	</body>
+</html>
